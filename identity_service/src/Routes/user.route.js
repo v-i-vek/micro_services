172e3c6cd87b438c3a7361e167ed1ef2d756fb25
v-i-vek <yadavvivek7698@gmail.com>
2025-08-24 23:20:34 +0530

    added refresh and logout api

--- identity_service/src/Routes/user.route.js
+++ identity_service/src/Routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {addUser,loginUser} = require('../controller/user.controller')
+const {addUser,loginUser,refreshToken} = require('../controller/user.controller')
 
 const router = express.Router();
 
 
 router.post('/register',addUser)
 router.post('/login',loginUser)
+router.post('/refresh-token',refreshToken)
 
 module.exports = router
--- identity_service/src/controller/user.controller.js
+++ identity_service/src/controller/user.controller.js
@@ -1,5 +1,8 @@
 const userModel = require('../Models/user')
-const { createToken } = require('../utils/generateToken')
+const RefreshTokenModel = require('../Models/refreshToken')
+
+const { createToken } = require('../utils/generateToken');
+const refreshTokenModel = require('../Models/refreshToken');
 
 
 const addUser = async (req, res) => {
@@ -39,4 +42,35 @@ const loginUser = async (req, res) => {
     }
 }
 
-module.exports = { addUser, loginUser }
+const refreshToken = async(req,res)=>{
+    try {
+        
+        const refreshToken = req.body.refreshToken;
+        if(!refreshToken) return res.status(400).json({message : "Invalid Request"})
+        
+        const storedToken = await refreshTokenModel.deleteOne({token:refreshToken})
+        if(!storedToken) return res.status(400).json({message : "Invalid Request"})
+        if(storedToken.expiresAt < new Date()){
+            return res.status(400).json({message : "Refresh Token Expired"})
+        }
+        const user = await userModel.findById(storedToken.userId)
+        if(!user) return res.status(400).json({message : "User not found"})
+        
+        const {accessToken,refreshToken:newRefreshToken} = await createToken(user)
+        return res.status(200).json({message : "Success",data:{accessToken,newRefreshToken}})
+
+    } catch (error) {
+        throw error
+    }
+}
+const logoutUser = async(req,res)=>{
+    try {
+        const refreshToken = req.body.refreshToken;
+        const deleteToken = await RefreshTokenModel.findOneAndDelete({token:refreshToken})
+        if(!deleteToken) return res.status(400).json({message : "Invalid Request"})
+        return res.status(200).json({message : "Logged out successfully"})
+    } catch (error) {
+        throw error
+    }
+}
+module.exports = { addUser, loginUser,refreshToken,logoutUser }
