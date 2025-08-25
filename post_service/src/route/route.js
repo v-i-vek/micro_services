8bdf75a422ff2dbb07664148765cc1cb277554b5
v-i-vek <yadavvivek7698@gmail.com>
2025-08-25 19:43:32 +0530

    added redis cached functionality

--- post_service/src/route/route.js
+++ post_service/src/route/route.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const{addBlog} = require('../controller/blog.controller')
+const{addBlog,allBlogs,singleBlog} = require('../controller/blog.controller')
 const{authenticateReq} = require('../middleware/auth.middleware')
 
 
 router.use(authenticateReq)
 router.post('/add-blog',addBlog)
+router.get('/all-blogs',allBlogs)
+router.get('/:id',singleBlog)
 
 module.exports = router
--- post_service/src/server.js
+++ post_service/src/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const redis = require('ioredis')
 
 const routes = require('./route/route')
 
@@ -11,14 +10,10 @@ const routes = require('./route/route')
 const PORT = process.env.PORT || 3002
 
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connected")).catch((err) => console.log(err));
-const redisClient = new redis(process.env.REDIS_URL)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use("/api/blog",(req, res, next) => {
-    req.redisClient = redisClient;
-    next();
-  },routes)
+app.use("/api/blog",routes)
 
 
 
