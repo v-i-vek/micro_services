d7864c7911ff4585437fc53c165cbd9ec82589db
v-i-vek <yadavvivek7698@gmail.com>
2025-08-28 15:41:32 +0530

    optmized delete query

--- media_service/src/event-handler/deleteBlogEvent.event.js
+++ media_service/src/event-handler/deleteBlogEvent.event.js
@@ -1,22 +1,25 @@
-const MediaModel = require('../model/Media')
-const {deleteFileFromS3} = require('../service/s3Client.service')
+const MediaModel = require("../model/Media");
+const { deleteFileFromS3 } = require("../service/s3Client.service");
 
-const handleBlogDeleted = async(content)=>{
-    try {
-        console.log("Event : \n",content)
+const handleBlogDeleted = async (content) => {
+  try {
+    console.log("Event : \n", content);
 
-
-        const{userId,mediaUrl} = content
-        for(let media of mediaUrl){
-            const blog = await MediaModel.findOneAndDelete({_id:media.blogId,user:userId})
-            console.log("====blog to delete", blog)
-            await deleteFileFromS3(blog.s3ObjectKey)
-        }
-        return "success"
-    } catch (error) {
-        console.log("Error while exeucting handleBlogDeleted \n", error)
-        throw error
+    const { userId, mediaUrl } = content;
+    for (let media of mediaUrl) {
+      const blog = await MediaModel.findOneAndDelete({
+        $and:[
+            {_id:media.blogId},
+            {user:userId}
+        ],
+      });
+      await deleteFileFromS3(blog.s3ObjectKey);
     }
-}
+    return "success";
+  } catch (error) {
+    console.log("Error while exeucting handleBlogDeleted \n", error);
+    throw error;
+  }
+};
 
-module.exports = {handleBlogDeleted}
+module.exports = { handleBlogDeleted };
